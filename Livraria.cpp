5c03abe55b60f1a6fb036ce313d2d1fe8032687e
Natan Melo <natan01melo@hotmail.com>
2022-03-15 20:22:30 -0300

    Add Input and Output

--- Livraria.cpp
+++ Livraria.cpp
@@ -4,29 +4,44 @@ using namespace std;
 
 class Livro {
     private:
-        int id;
+        int id = 0;
         string nome;
         string sinopse;
     public:
-        Livro(string nm, string sp){
+        Livro(){
             id = id + 1; 
-            nome = nm;
-            sinopse = sp;
         }
         int getId(); 
         string getNome();
         string getSinopse();
+        void setNome(string n);
+        void setSinopse(string s);
 };
 
 int Livro::getId(){ return id;  }
 string Livro::getNome(){ return nome; };
 string Livro::getSinopse(){ return sinopse; }
+void Livro::setNome(string n){ nome = n;}
+void Livro::setSinopse(string s){ sinopse = s; }
 
 int main()
 {
     cout << "Livraria Anhembi Morumbi" << endl;
-    Livro lv("Chapeuzinho vermelho", "Livro legal, muito bom");
-    cout << "Dados do livro: " << endl << lv.getNome();
+    Livro lv;
+    string nome_livro;
+    string sinopse_livro;
+
+    cout << "Digite o nome do Livro: " << endl;
+    getline(cin, nome_livro);
+    lv.setNome(nome_livro);
+    
+    cout << "Digite a sinopse deste livro: " << endl;
+    getline(cin, sinopse_livro);
+    lv.setSinopse(sinopse_livro);
+
+    cout << "Dados do livro: " << endl;
+    cout << lv.getId() << " - " << lv.getNome() << " - " << lv.getSinopse() << endl;
+
 
     return EXIT_SUCCESS;
 }
